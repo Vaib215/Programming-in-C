145e178dfb17e77f3b11b6ea3784ff5e24fa666f
Vaibhav Sisodiya <sisodiya.vaib215@gmail.com>
2021-12-06 15:03:23 +0530

    C files

---
+++ 2d_array_add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    //Author: VAIB
+    int a,b;
+    printf("Enter no. of rows and colums: ");
+    scanf("%d %d",&a,&b);
+    int arr1[a][b];
+    printf("Enter elements in 1st matrix\n");
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            scanf("%d",&arr1[i][j]);
+        }
+        
+    }
+    int arr2[a][b];
+    printf("Enter elements in 2nd matrix\n");
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            scanf("%d",&arr2[i][j]);
+        }
+        
+    }
+    //Sum
+    printf("Sum of the matrices is\n");
+    int arr3[a][b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            printf("%d ",arr1[i][j]+arr2[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
